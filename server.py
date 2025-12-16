8e12cb6236a9e1476f7e2436dec2a25f79ee48bd
richi <ricardostiven9@gmail.com>
2025-12-15 20:54:35 -0500

    ajuste cors

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ class ChatInput(BaseModel):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
